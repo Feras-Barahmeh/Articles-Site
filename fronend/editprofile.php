67ee45f76dc44503c79ff9187ef6acde9b488ae0
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2022-09-12 01:06:46 +0300

    Add Skilles Tags && and some edit in frontend in edit peofile page

--- fronend/editprofile.php
+++ fronend/editprofile.php
@@ -6,9 +6,11 @@
     $AllWidth = "null"; // To Set Width Nav 100%
     include ("init.php");
 
-
 // Main Structers
+
+
     function MainStructer () {
+        $user = Queries::FromTable("*", "users", "WHERE IdUser = {$_SESSION['IdUser']}", "fetch");
         ?>
             <div class="container-page">
                 <div class="layout">
@@ -19,8 +21,8 @@
                                 "profile-pictuer"); 
                     ?>
                     <div class="name">
-                        <div class="name-user">Majd Barahmeh</div>
-                        <div class="id-user"><span class="title">ID user : </span> <span>majd</span> </div>
+                        <div class="name-user"><?php echo $user['fullName'] ?></div>
+                        <div class="id-user"><span class="title">ID user : </span> <span><?php echo $user['userName'] ?></span> </div>
                     </div>
                 </div>
 
@@ -43,11 +45,16 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Name</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Feras</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['fullName']) ? $user['fullName'] : "Your Full Name" ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Name">
+                                                        <input 
+                                                        type="text" 
+                                                        name="name" 
+                                                        value="<?php echo isset($user['fullName']) ? $user['fullName'] : "Your Full Name" ?>" 
+                                                        class="snippet-input" 
+                                                        placeholder="Name">
                                                         <div class="btns"><button class="save" name="save-edit" id="save-edit">Save</button><button class="cancel" id="cancel">Cancel</button></div>
                                                     </div>
                                                 </div>
@@ -64,14 +71,14 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Gender</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">male</span>
+                                                <span class="containt-reg-db"><?php echo $user['gender'] == "m" ? "Mael" : "Femal" ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
                                                         <select name="" id="" class="snippet-input">
-                                                            <option value="">Gender</option>
-                                                            <option value="">Mael</option>
-                                                            <option value="">Femal</option>
+                                                            <option value="<?php echo isset($user['gender']) && $user['gender'] == "m" ? "Mael" : "Femal" ?>"><?php echo $user['gender'] == "m" ? "Mael" : "Femal" ?></option>
+                                                            <option value="m">Mael</option>
+                                                            <option value="f">Femal</option>
                                                         </select>
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
@@ -89,11 +96,11 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Location</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Location</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['location']) ? $user['location'] : "City - Country"  ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Location">
+                                                        <input type="text" name="" value="<?php echo isset($user['location']) ? $user['location'] : "City - Country"  ?>" class="snippet-input" placeholder="Location">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -110,11 +117,11 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Birthday</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Birthday</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['age']) ? $user['age'] : "Your Barthday"  ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Birthday">
+                                                        <input type="date" name="" value="<?php echo isset($user['age']) ? $user['age'] : "Your Barthday"  ?>" class="snippet-input" placeholder="Birthday">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -131,11 +138,11 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Brief</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Brief</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['aboutYou']) ? $user['aboutYou'] : "Brief To You"  ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <textarea name="" id=""  placeholder="Brief" cols="50" rows="5"></textarea>
+                                                        <textarea name="" id=""  placeholder="Brief" cols="50" rows="5"><?php echo isset($user['aboutYou']) ? $user['aboutYou'] : "Brief To You"  ?></textarea>
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -152,11 +159,14 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Website</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Website</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['website']) ? $user['website'] : "Your website | Blog" ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Website">
+                                                        <input type="text" name=""
+                                                                value=""
+                                                                class="snippet-input"
+                                                                placeholder="<?php if ( ! isset($user['website']) ) echo  "Yore Site | Bloge" ?>">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -173,11 +183,11 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Githup</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Githup</span>
+                                                <span class="containt-reg-db"><?php if ( isset($user['githup']) ) echo  $user['githup']; ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Githup">
+                                                        <input type="text" name="" value="<?php if ( isset($user['githup']) ) echo  $user['githup']; ?>" class="snippet-input" placeholder="Githup">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -194,11 +204,11 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Twitter</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Twitter</span>
+                                                <span class="containt-reg-db"><?php if ( isset($user['twitter']) ) echo  $user['twitter']; ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Twitter">
+                                                        <input type="text" name="" value="<?php if ( isset($user['twitter']) ) echo  $user['twitter']; ?>" class="snippet-input" placeholder="Twitter">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -215,11 +225,16 @@
                                         <div class="row-feild">
                                             <div class="name-feild">Linkedin</div>
                                             <div class="content-feild">
-                                                <span class="containt-reg-db">Linkedin</span>
+                                                <span class="containt-reg-db"><?php echo isset($user['linkedin']) ?  $user['linkedin'] : "Your Linkedin Account" ?></span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
                                                     <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input" placeholder="Linkedin">
+                                                        <input 
+                                                        type="text" 
+                                                        name="" 
+                                                        value="<?php echo isset($user['linkedin']) ?  $user['linkedin'] : "Your Linkedin Account" ?>" 
+                                                        class="snippet-input"
+                                                        placeholder="Linkedin">
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -240,11 +255,15 @@
                                             <div class="row-feild">
                                                 <div class="name-feild">Education</div>
                                                 <div class="content-feild">
-                                                    <span class="containt-reg-db">Education</span>
+                                                    <span class="containt-reg-db"><?php echo isset($user['education']) ?  $user['education'] : "Education" ?></span>
                                                     <!-- Start input Feils -->
                                                     <div class="contanier-proccess hidden">
                                                         <div class="">
-                                                            <input type="text" name="" value="" class="snippet-input" placeholder="Education">
+                                                            <input 
+                                                            type="text" 
+                                                            name="" value="<?php echo isset($user['education']) ?  $user['education'] : "Education" ?>" 
+                                                            class="snippet-input" 
+                                                            placeholder="Education">
                                                             <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                         </div>
                                                     </div>
@@ -260,7 +279,7 @@
                                             <div class="row-feild">
                                                 <div class="name-feild">Work</div>
                                                 <div class="content-feild">
-                                                    <span class="">Work</span>
+                                                    <span class=""><?php echo isset($user['work']) ?  $user['work'] : "Work At" ?></span>
                                                     <!-- Start input Feils -->
                                                     <div class="contanier-proccess hidden">
                                                         <div class="">
@@ -289,8 +308,10 @@
                                                 <span class="containt-reg-db">skills</span>
                                                 <!-- Start input Feils -->
                                                 <div class="contanier-proccess hidden">
-                                                    <div class="">
-                                                        <input type="text" name="" value="" class="snippet-input input-shills" placeholder="skills">
+                                                    <div class="" id="">
+                                                        <div id="tag-contanier">
+                                                            <input type="text" name="" value="" id="skills" class="snippet-input input-shills" placeholder="skills">
+                                                        </div>
                                                         <div class="btns"><span class="save">Save</span><span class="cancel">Cancel</span></div>
                                                     </div>
                                                 </div>
@@ -308,8 +329,6 @@
             </div>
         <?php
     }
-
-
 
 // Controller 
     MainStructer();
--- fronend/layout/js/editprofile.js
+++ fronend/layout/js/editprofile.js
@@ -45,3 +45,73 @@
                 }
             });
         });
+
+
+        // When Clikc Cancel  Btn hidden form
+        const cancelBtns = document.querySelectorAll(".cancel");
+
+        if (cancelBtns !== null) {
+            cancelBtns.forEach((cancelBtn) => {
+                cancelBtn.addEventListener("click", ()=> {
+                    cancelBtn.closest(".contanier-proccess").classList.add("hidden")
+                    cancelBtn.closest(".content-feild").firstElementChild.classList.toggle("hidden");
+                });
+            });
+        }
+
+        // Creat Skille Tage
+            function CreatTag(label) {
+                const div = document.createElement("div");
+                div.setAttribute("class", "tag");
+                const span = document.createElement("span");
+                span.innerHTML = label;
+                const closeBtn = document.createElement("i");
+
+                closeBtn.setAttribute("class", "fa fa-xmark");
+                closeBtn.setAttribute("data-tag", label);
+                div.appendChild(span);
+                div.appendChild(closeBtn);
+
+                return div;
+            }
+
+            function remDublication() {
+                const alltags = document.querySelectorAll(".tag");
+                alltags.forEach((tag)=>{
+                    tag.parentElement.removeChild(tag);
+                });
+            }
+
+            function addTags() {
+                remDublication();
+                tags.slice().reverse().forEach((tag)=> {
+                    containerTags.prepend(CreatTag(tag));
+                });
+            }
+
+            const containerTags = document.getElementById("tag-contanier");
+            const skille = document.getElementById("skills");
+            var tags = [];
+
+            // Fetch Words
+            skille.addEventListener("keyup", (e)=> {
+                if (e.key === 'Enter' || e.keyCode === 13) {
+                    let tag = e.target.value;
+                    if (tag.length >= 1) {
+                        tags.push(tag);
+                    }
+                    addTags();
+                    tag = e.target.value = "";
+                }
+            });
+
+            // Remove Tags When Click X btn
+            document.addEventListener("click", (e)=>{
+                if (e.target.tagName === "I") {
+                    let data = e.target.getAttribute("data-tag")
+                    const index = tags.indexOf(data);
+                    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+                    addTags();
+                }
+            });
+        
